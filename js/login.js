df7aa2aa31bf009f5899c662e7f4684e313fef74
kiddopro <ms.prgramador@gmail.com>
2021-08-09 18:25:03 -0300

    feature: login added and a popup for logout in index

--- js/login.js
+++ js/login.js
@@ -1,6 +1,31 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener('DOMContentLoaded', function (e) {});
 
-});
+userLogin = () => {
+  let user = document.getElementById('name').value;
+  let pass = document.getElementById('pass').value;
+
+  if (user.trim() === '') {
+    alert('You need at least an username');
+  } else if (pass.trim() === '') {
+    localStorage.setItem('username', user);
+    localStorage.setItem('typeUser', 'guest');
+  } else {
+    localStorage.setItem('username', user);
+    localStorage.setItem('password', pass);
+    localStorage.setItem('typeUser', 'member');
+  }
+
+  console.log(
+    'User: ' +
+      localStorage.getItem('username') +
+      ' Password: ' +
+      localStorage.getItem('password') +
+      ' TypeUser: ' +
+      localStorage.getItem('typeUser')
+  );
+
+  location.href = 'index.html';
+};
--- js/products.js
+++ js/products.js
@@ -1,6 +1,44 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+document.addEventListener('DOMContentLoaded', function (e) {
+  getJSONData(PRODUCTS_URL).then((response) => {
+    let elemento = '';
 
+    for (let i = 0; i < response.data.length; i++) {
+      let dato = response.data[i];
+      elemento +=
+        `<a href="#" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` +
+        dato.imgSrc +
+        `" alt="` +
+        dato.description +
+        `" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">` +
+        dato.name +
+        `</h4>
+                            <small class="text-muted">` +
+        dato.soldCount +
+        ` artículos</small>
+                        </div>
+                        <p class="mb-1">` +
+        dato.description +
+        `</p>
+        <small class="text-muted">` +
+        dato.cost +
+        ` ` +
+        dato.currency +
+        `</small>
+                    </div>
+                </div>
+            </a>`;
+
+      document.getElementById('product-list-container').innerHTML = elemento;
+    }
+  });
 });
