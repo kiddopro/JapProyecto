75748757554d4dfbd45c7353423fa22d61bfcb9b
kiddopro <ms.prgramador@gmail.com>
2021-08-12 12:09:42 -0300

    fixed login 2

--- js/init.js
+++ js/init.js
@@ -46,13 +46,15 @@ var getJSONData = function (url) {
     });
 };
 
+const disconnect = () => {
+  localStorage.clear();
+  location.href = 'login.html';
+};
 
-function disconnect() {
+function signOut() {
   var auth2 = gapi.auth2.getAuthInstance();
-  auth2.disconnect().then(()=>{
+  auth2.signOut().then(function () {
     console.log('User signed out.');
-    localStorage.clear();
-    location.href = 'login.html';
 });
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
