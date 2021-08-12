ef1c282ec40065981147994e79f4c5c9170a87c3
kiddopro <ms.prgramador@gmail.com>
2021-08-12 03:18:31 -0300

    testing sign in with google account

--- js/login.js
+++ js/login.js
@@ -4,16 +4,21 @@
 document.addEventListener('DOMContentLoaded', function (e) {});
 
 userLogin = () => {
+  let alert = document.getElementById('alert-messsage');
+  alert.innerHTML = 'You need at least an username';
+  alert.style.color = '#E50000';
   let user = document.getElementById('name').value;
   let pass = document.getElementById('pass').value;
 
   if (user.trim() === '') {
-    alert('You need at least an username');
+    alert.style.display = 'inline';
   } else if (pass.trim() === '') {
+    alert.style.display = 'none';
     localStorage.setItem('username', user);
     localStorage.setItem('typeUser', 'guest');
     location.href = 'index.html';
   } else {
+    alert.style.display = 'none';
     localStorage.setItem('username', user);
     localStorage.setItem('password', pass);
     localStorage.setItem('typeUser', 'member');
@@ -29,3 +34,25 @@ userLogin = () => {
       localStorage.getItem('typeUser')
   );
 };
+
+function onSignIn(googleUser) {
+  // Useful data for your client-side scripts:
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Don't send this directly to your server!
+  console.log('Full Name: ' + profile.getName());
+  console.log('Given Name: ' + profile.getGivenName());
+  console.log('Family Name: ' + profile.getFamilyName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
+
+  // The ID token you need to pass to your backend:
+  var id_token = googleUser.getAuthResponse().id_token;
+  console.log('ID Token: ' + id_token);
+}
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
+}
