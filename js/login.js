e855ea80b52418ed596823c8c5ff86c6b3e5c51d
kiddopro <ms.prgramador@gmail.com>
2021-08-14 00:10:01 -0300

    finish login

--- js/login.js
+++ js/login.js
@@ -33,7 +33,7 @@ function onSignIn(googleUser) {
   console.log('Given Name: ' + profile.getGivenName());
   console.log('Family Name: ' + profile.getFamilyName());
   console.log('Image URL: ' + profile.getImageUrl());
-  sessionStorage('Email: ' + profile.getEmail());
+  sessionStorage('email', profile.getEmail());
 
   // The ID token you need to pass to your backend:
   var id_token = googleUser.getAuthResponse().id_token;
