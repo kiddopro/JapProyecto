f8e1b2a7b1d5f0b52ba8f8a2ef1668f625a03ef2
kiddopro <ms.prgramador@gmail.com>
2021-08-14 00:01:59 -0300

    finish login

--- js/login.js
+++ js/login.js
@@ -52,7 +52,7 @@ function onSignIn(googleUser) {
 }
 
 function signOut() {
-  var auth2 = gapi.auth2.getAuthInstance().disconnect();
+  var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function () {
     console.log('User signed out.');
     location.href = 'login.html';
