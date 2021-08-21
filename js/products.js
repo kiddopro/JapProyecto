d57a2e0a355381ed084bb9e263ea42adc16ca9b0
kiddopro <ms.prgramador@gmail.com>
2021-08-21 04:31:22 -0300

    desafiate del entrega 2 finished

--- js/products.js
+++ js/products.js
@@ -214,8 +214,9 @@ document.addEventListener('DOMContentLoaded', function (e) {
         search = document.getElementById('searchInput').value;
         console.log(search);
         showCategoriesList2();
+      } else if (event.code === '') {
+        showCategoriesList();
       }
-      console.log(event.code);
     });
 
   document.getElementById('sortAsc').addEventListener('click', function () {
