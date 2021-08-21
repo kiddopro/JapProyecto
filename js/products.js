35e193e08499f58fdfb9a13205a6ac1e53170f60
kiddopro <ms.prgramador@gmail.com>
2021-08-21 02:23:19 -0300

    punto 1, 2 y 3 del entrega 2 finished

--- js/products.js
+++ js/products.js
@@ -75,7 +75,7 @@ function showCategoriesList() {
     ) {
       htmlContentToAppend +=
         `
-            <a href="category-info.html" class="list-group-item list-group-item-action">
+            <a href="product-info.html" class="list-group-item list-group-item-action">
                 <div class="row">
                     <div class="col-3">
                         <img src="` +
