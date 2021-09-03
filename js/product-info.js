abc26dd7fea19255675647e02eb3550906c3ac79
kiddopro <ms.prgramador@gmail.com>
2021-09-03 16:37:32 -0300

    added name and description of the product-info

--- js/product-info.js
+++ js/product-info.js
@@ -1,6 +1,15 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener('DOMContentLoaded', function (e) {});
 
-});
+let productName = document.querySelector('#productName');
+let productDescription = document.querySelector('#productDescription');
+getJSONData(PRODUCT_INFO_URL)
+  .then(function (result) {
+    let product = result.data;
+    console.log(result.data);
+    productName.innerHTML = product.name;
+    productDescription.innerHTML = product.description;
+  })
+  .catch((error) => console.log(error));
