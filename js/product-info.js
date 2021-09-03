f77563d74fc600e2521572d6c274f7bd73fee470
kiddopro <ms.prgramador@gmail.com>
2021-09-03 16:48:57 -0300

    added cost, soldcount and category of the product

--- js/product-info.js
+++ js/product-info.js
@@ -5,11 +5,17 @@ document.addEventListener('DOMContentLoaded', function (e) {});
 
 let productName = document.querySelector('#productName');
 let productDescription = document.querySelector('#productDescription');
+let productSoldCount = document.querySelector('#productSoldCount');
+let productCost = document.querySelector('#productCost');
+let productCategory = document.querySelector('#productCategory');
 getJSONData(PRODUCT_INFO_URL)
   .then(function (result) {
     let product = result.data;
     console.log(result.data);
     productName.innerHTML = product.name;
     productDescription.innerHTML = product.description;
+    productSoldCount.innerHTML = product.soldCount;
+    productCost.innerHTML = product.cost;
+    productCategory.innerHTML = product.category;
   })
   .catch((error) => console.log(error));
