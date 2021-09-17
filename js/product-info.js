55689f6cd8bc79e647804c822344feb0998b0e1b
kiddopro <ms.prgramador@gmail.com>
2021-09-16 23:03:15 -0300

    finished

--- js/product-info.js
+++ js/product-info.js
@@ -13,6 +13,8 @@ let productCarusel = document.querySelector('.carousel-inner');
 let productComments = document.querySelector('#productComments');
 let comment = document.querySelector('#inputComment');
 let calificacion = document.querySelector('#calification');
+let checked = "<small class='fa fa-star checked'></small>";
+let notChecked = "<small class='fa fa-star'></small>";
 getJSONData(PRODUCT_INFO_URL)
   .then(function (result) {
     let product = result.data;
@@ -21,21 +23,8 @@ getJSONData(PRODUCT_INFO_URL)
     productSoldCount.innerHTML = product.soldCount;
     productCost.innerHTML = product.cost;
     productCategory.innerHTML = product.category;
-    // let elementoQueContieneTodasLasImagenes = '';
-    // for (let i = 0; i < product.images.length; i++) {
-    //   let img = product.images[i];
-    //   elementoQueContieneTodasLasImagenes +=
-    //     `
-    //     <div class="col-lg-3 col-md-4 col-6">
-    //         <div class="d-block mb-4 h-100">
-    //             <img class="img-fluid img-thumbnail" src="` +
-    //     img +
-    //     `" alt="">
-    //         </div>
-    //     </div>
-    //     `;
-    // }
-    // productImages.innerHTML = elementoQueContieneTodasLasImagenes;
+
+    //carrousel de imagenes que marca activo a la primera imágen
     let carousel = '';
     for (let i = 0; i < product.images.length; i++) {
       let img = product.images[i];
@@ -60,107 +49,27 @@ getJSONData(PRODUCT_INFO_URL)
   })
   .catch((error) => console.log(error));
 
+//se cargan los comentarios a la pagina
 getJSONData(PRODUCT_INFO_COMMENTS_URL)
   .then((comments) => {
     let comentarios = comments.data;
+
     let e = '';
     for (let i = 0; i < comentarios.length; i++) {
-      if (comentarios[i].score === 1) {
-        e +=
-          `<div id='starsContainer'>` +
-          `
-            <span class="fa fa-star checked"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>` +
-          `</div><span id='commentUser'>` +
-          comentarios[i].user +
-          `</span><p id='commentDescription' class='m-0'>` +
-          comentarios[i].description +
-          `</p><span id='commentDate'>` +
-          comentarios[i].dateTime +
-          `</span><br><br>`;
-      } else if (comentarios[i].score === 2) {
-        e +=
-          `<div id='starsContainer'>` +
-          `
-            <span class="fa fa-star checked"></span>
-            <span class="fa fa-star checked"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>` +
-          `</div><span id='commentUser'>` +
-          comentarios[i].user +
-          `</span><p id='commentDescription' class='m-0'>` +
-          comentarios[i].description +
-          `</p><span id='commentDate'>` +
-          comentarios[i].dateTime +
-          `</span><br><br>`;
-      } else if (comentarios[i].score === 3) {
-        e +=
-          `<div id='starsContainer'>` +
-          `
-            <span class="fa fa-star checked"></span>
-            <span class="fa fa-star checked"></span>
-            <span class="fa fa-star checked"></span>
-            <span class="fa fa-star"></span>
-            <span class="fa fa-star"></span>` +
-          `</div><span id='commentUser'>` +
-          comentarios[i].user +
-          `</span><p id='commentDescription' class='m-0'>` +
-          comentarios[i].description +
-          `</p><span id='commentDate'>` +
-          comentarios[i].dateTime +
-          `</span><br><br>`;
-      } else if (comentarios[i].score === 4) {
-        e +=
-          `<div id='starsContainer'>` +
-          `
-            <span class="fa fa-star checked"></span>
-            <span class="fa fa-star checked"></span>
-            <span class="fa fa-star checked"></span>
-            <span class="fa fa-star checked"></span>
-            <span class="fa fa-star"></span>` +
-          `</div><span id='commentUser'>` +
-          comentarios[i].user +
-          `</span><p id='commentDescription' class='m-0'>` +
-          comentarios[i].description +
-          `</p><span id='commentDate'>` +
-          comentarios[i].dateTime +
-          `</span><br><br>`;
-      } else {
-        e +=
-          `<div id='starsContainer'>` +
-          `
-            <span class="fa fa-star checked"></span>
-            <span class="fa fa-star checked"></span>
-            <span class="fa fa-star checked"></span>
-            <span class="fa fa-star checked"></span>
-            <span class="fa fa-star checked"></span>` +
-          `</div><span id='commentUser'>` +
-          comentarios[i].user +
-          `</span><p id='commentDescription' class='m-0'>` +
-          comentarios[i].description +
-          `</p><span id='commentDate'>` +
-          comentarios[i].dateTime +
-          `</span><br><br>`;
-      }
-      //   e +=
-      //     `<div id='starsContainer'>` +
-      //     `
-      //         <span class="fa fa-star checked"></span>
-      //         <span class="fa fa-star"></span>
-      //         <span class="fa fa-star"></span>
-      //         <span class="fa fa-star"></span>
-      //         <span class="fa fa-star"></span>` +
-      //     `</div><span id='commentUser'>` +
-      //     comentarios[i].user +
-      //     `</span><p id='commentDescription' class='m-0'>` +
-      //     comentarios[i].description +
-      //     `</p><span id='commentDate'>` +
-      //     comentarios[i].dateTime +
-      //     `</span><br><br>`;
+      let puntaje = comentarios[i].score;
+      let puntajeChecked = checked.repeat(puntaje);
+      let puntajeNotChecked = notChecked.repeat(5 - puntaje);
+      e +=
+        `<div id='starsContainer'>` +
+        puntajeChecked +
+        puntajeNotChecked +
+        `</div><span id='commentUser'>` +
+        comentarios[i].user +
+        `</span><p id='commentDescription' class='m-0'>` +
+        comentarios[i].description +
+        `</p><span id='commentDate'>` +
+        comentarios[i].dateTime +
+        `</span><br><br>`;
     }
     productComments.innerHTML = e;
   })
@@ -170,9 +79,6 @@ getJSONData(PRODUCT_INFO_COMMENTS_URL)
 document.getElementById('btnComentar').addEventListener('click', (event) => {
   agregarComentario();
 });
-
-//para mas adelante
-function cargarComentarios(score) {}
 
 function agregarComentario() {
   //obtenemos la fecha
@@ -187,87 +93,23 @@ function agregarComentario() {
   div.id = 'starsContainer';
   let comentario = comment.value;
   let puntaje = calificacion.value;
+  let puntajeChecked = checked.repeat(puntaje);
+  let puntajeNotChecked = notChecked.repeat(5 - puntaje);
   let usuarioComenta = sessionStorage.getItem('username')
     ? sessionStorage.getItem('username')
     : sessionStorage.getItem('email');
 
   if (comentario) {
-    if (puntaje === '1') {
-      div.innerHTML =
-        `
-      <span class="fa fa-star checked"></span>
-      <span class="fa fa-star"></span>
-      <span class="fa fa-star"></span>
-      <span class="fa fa-star"></span>
-      <span class="fa fa-star"></span>` +
-        `</div><br><span id='commentUser'>` +
-        usuarioComenta +
-        `</span><p id='commentDescription' class='m-0'>` +
-        comentario +
-        `</p><span id='commentDate'>` +
-        fechaCompleta +
-        `</span><br><br>`;
-    } else if (puntaje === '2') {
-      div.innerHTML =
-        `
-      <span class="fa fa-star checked"></span>
-      <span class="fa fa-star checked"></span>
-      <span class="fa fa-star"></span>
-      <span class="fa fa-star"></span>
-      <span class="fa fa-star"></span>` +
-        `</div><br><span id='commentUser'>` +
-        usuarioComenta +
-        `</span><p id='commentDescription' class='m-0'>` +
-        comentario +
-        `</p><span id='commentDate'>` +
-        fechaCompleta +
-        `</span><br><br>`;
-    } else if (puntaje === '3') {
-      div.innerHTML =
-        `
-      <span class="fa fa-star checked"></span>
-      <span class="fa fa-star checked"></span>
-      <span class="fa fa-star checked"></span>
-      <span class="fa fa-star"></span>
-      <span class="fa fa-star"></span>` +
-        `</div><br><span id='commentUser'>` +
-        usuarioComenta +
-        `</span><p id='commentDescription' class='m-0'>` +
-        comentario +
-        `</p><span id='commentDate'>` +
-        fechaCompleta +
-        `</span><br><br>`;
-    } else if (puntaje === '4') {
-      div.innerHTML =
-        `
-      <span class="fa fa-star checked"></span>
-      <span class="fa fa-star checked"></span>
-      <span class="fa fa-star checked"></span>
-      <span class="fa fa-star checked"></span>
-      <span class="fa fa-star"></span>` +
-        `</div><br><span id='commentUser'>` +
-        usuarioComenta +
-        `</span><p id='commentDescription' class='m-0'>` +
-        comentario +
-        `</p><span id='commentDate'>` +
-        fechaCompleta +
-        `</span><br><br>`;
-    } else {
-      div.innerHTML =
-        `
-      <span class="fa fa-star checked"></span>
-      <span class="fa fa-star checked"></span>
-      <span class="fa fa-star checked"></span>
-      <span class="fa fa-star checked"></span>
-      <span class="fa fa-star checked"></span>` +
-        `</div><br><span id='commentUser'>` +
-        usuarioComenta +
-        `</span><p id='commentDescription' class='m-0'>` +
-        comentario +
-        `</p><span id='commentDate'>` +
-        fechaCompleta +
-        `</span><br><br>`;
-    }
+    div.innerHTML =
+      puntajeChecked +
+      puntajeNotChecked +
+      `</div><br><span id='commentUser'>` +
+      usuarioComenta +
+      `</span><p id='commentDescription' class='m-0'>` +
+      comentario +
+      `</p><span id='commentDate'>` +
+      fechaCompleta +
+      `</span><br><br>`;
   } else {
     comment.style.border = '1px solid red';
   }
