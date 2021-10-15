8032598127f9049a69ba14f652deb947d91df9c1
kiddopro <ms.prgramador@gmail.com>
2021-10-15 13:48:14 -0300

    entrega 5 finished

--- js/cart.js
+++ js/cart.js
@@ -1,38 +1,62 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+const IVA = 0.22;
 let cantidad = 0;
 let costo = 0;
-const addProduct = () => {
+let subtotal = 0;
+let total = 0;
+let iva = 0;
+const addProduct = (id) => {
   let elementoCantidad = document.getElementById('cart_count');
+  let elementoSubTotal = document.getElementById('cart_subtotal');
   let elementoTotal = document.getElementById('cart_total');
+  let elementoIva = document.getElementById('cart_iva');
   elementoCantidad.innerHTML = cantidad += 1;
-  elementoTotal.innerHTML = costo * cantidad;
-  console.log(cantidad);
+  subtotal = costo * cantidad;
+  iva = subtotal * IVA;
+  total = subtotal * IVA + subtotal;
+  elementoSubTotal.innerHTML = subtotal;
+  elementoIva.innerHTML = iva;
+  elementoTotal.innerHTML = total;
 };
 
-const deleteProduct = () => {
+const deleteProduct = (id) => {
   let elementoCantidad = document.getElementById('cart_count');
+  let elementoSubTotal = document.getElementById('cart_subtotal');
   let elementoTotal = document.getElementById('cart_total');
+  let elementoIva = document.getElementById('cart_iva');
   elementoCantidad.innerHTML = cantidad > 1 ? (cantidad -= 1) : cantidad;
-  elementoTotal.innerHTML = costo * cantidad;
+  subtotal = costo * cantidad;
+  iva = subtotal * IVA;
+  total = subtotal * IVA + subtotal;
+  elementoSubTotal.innerHTML = subtotal;
+  elementoIva.innerHTML = iva;
+  elementoTotal.innerHTML = total;
 };
 
 document.addEventListener('DOMContentLoaded', function (e) {
   let contenedor = document.getElementById('contenedor-tarjeta');
+  let contenedor2 = document.getElementById('contenedor-total');
   let htmlText = '';
+  let htmlTotal = '';
   fetch(CART_INFO_URL)
     .then((res) => res.json())
     .then((data) => {
       data.articles.map((item, index) => {
         cantidad = item.count;
         costo = item.unitCost;
+        subtotal = costo * cantidad;
+        total = subtotal * IVA + subtotal;
+        iva = subtotal * IVA;
         htmlText +=
           `<div id="` +
           index +
           `"class="cart container-fluid d-flex border p-3">
         <div
           class="
+            border-secondary
+            border-right
             left-side
             w-25
             d-flex
@@ -44,7 +68,7 @@ document.addEventListener('DOMContentLoaded', function (e) {
           item.src +
           `" alt="" srcset="" style="width:100%; height:150px; object-fit: contain"/>
         </div>
-        <div class="right-side w-75">
+        <div class="right-side w-75 ml-5">
           <div class="cart-header d-flex justify-content-between mb-3">
             <span class="font-weight-bold">` +
           item.name +
@@ -93,7 +117,11 @@ document.addEventListener('DOMContentLoaded', function (e) {
           </div>
         </div>
       </div>
-      <hr />
+      `;
+      });
+      contenedor.innerHTML = htmlText;
+      htmlTotal =
+        `<hr />
       <div
         class="
           container-fluid
@@ -104,16 +132,47 @@ document.addEventListener('DOMContentLoaded', function (e) {
           buy-part
         "
       >
-        <i class="fas fa-dollar-sign"> Total:</i>
-
+      <div class="">
+        <i class="fas fa-dollar-sign">Subtotal:</i>
+        <span id="cart_subtotal" class="ml-1">` +
+        subtotal +
+        `</span><br/>
+        <i class="fas fa-dollar-sign">IVA:</i>
+        <span id="cart_iva" class="ml-1">` +
+        iva +
+        `</span><br/>
+        <i class="fas fa-dollar-sign">Total:</i>
         <span id="cart_total" class="ml-1">` +
-          costo * cantidad +
-          `</span>
-        <button class="ml-5 rounded p-2 bg-success text-white">✔Terminar</button>
+        total +
+        `</span>
+        </div>
+        <div>
+        <button class="ml-5 rounded p-2 bg-success text-white" data-toggle="modal" data-target="#exampleModal">✔Terminar</button>
         <button class="ml-1 rounded p-2 bg-danger text-white">❌Cancelar</button>
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Purchase</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Está apunto de comprar el producto con un costo de $ ` +
+        total +
+        `
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+        <button type="button" class="btn btn-primary">Comprar</button>
+      </div>
+    </div>
+  </div>
+</div>
+        </div>
       </div>`;
-      });
-      contenedor.innerHTML = htmlText;
+      contenedor2.innerHTML = htmlTotal;
     })
     .catch((err) => err);
 });
