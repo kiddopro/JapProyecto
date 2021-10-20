9c092d78a731b7461a7ec83106740fb05823eebf
kiddopro <ms.prgramador@gmail.com>
2021-10-20 12:21:48 -0300

    almost finished

--- js/my-profile.js
+++ js/my-profile.js
@@ -6,17 +6,28 @@ document.addEventListener('DOMContentLoaded', function (e) {
     'user_profile'
   ).innerHTML = `<div class="card" style="width: 18rem;">
   <div class="">
-  <img src="https://picsum.photos/536/354" class="card-img-top" alt="...">
+  <img src="https://picsum.photos/536/354" class="card-img-top  w-100 img-card" alt="...">
   </div>
   <div class="card-body">
   <div class="nombre d-flex justify-content-between align-items-center m-1">
-    <input class="card-title border-0 m-0" readonly value="${sessionStorage.getItem(
+    <input id="input1" class="card-title border-0 m-0" readonly value="${sessionStorage.getItem(
       'name'
-    )}"/><i class="fas fa-edit"></i></div>
+    )}"/><i class="fas fa-edit" onclick="edit(1)"></i></div>
     <div class="descripcion d-flex justify-content-between align-items-center m-1">
-    <input class="card-text border-0 m-0" value="lorem ipsum" readonly /><i class="fas fa-edit cursor"></i>
+    <input id="input2" class="card-text border-0 m-0" value="lorem ipsum" readonly /><i class="fas fa-edit cursor" onclick="edit(2)"></i>
     </div>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>`;
 });
+
+function edit(id) {
+  console.log(id);
+  let elemento = document.getElementById(`input${id}`);
+  elemento.focus();
+  elemento.removeAttribute('readonly');
+  console.log(elemento);
+  elemento.addEventListener('change', (e) => {
+    elemento.setAttribute('readonly', '');
+  });
+}
